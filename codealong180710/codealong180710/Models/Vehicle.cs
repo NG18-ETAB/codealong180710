b2f51d8745924eb6707802e49dd1b9edae379d3a
John Hellman <acoppero@gmail.com>
2018-07-10 09:06:18 +0200

    Init

---
+++ codealong180710/codealong180710/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace codealong180710.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NrOfWheels { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+    }
+}
